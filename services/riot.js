cae42a00df38709dc7d0266c214f33c4276b6db5
believer <rickard.laurin@gmail.com>
2016-05-01 15:00:37 +0200

    Some documentation for riot routes

--- services/riot.js
+++ services/riot.js
@@ -1,75 +1,16 @@
 const BASE = process.env.LOL_BASE
 const GLOBAL_BASE = process.env.LOL_GLOBAL
 const KEY = process.env.RIOT_API_KEY
-const fetch = require('../utils/fetch')
 const _ = require('lodash')
 const moment = require('moment')
+const fetch = require('../utils/fetch')
+const prepUrl = require('../utils/prepUrl')
 
-function prepUrl (url, region, base, options) {
-  region = region || 'eune'
-  base = base || BASE
-  options = options || ''
-
-  base = base.replace('{region}', region)
-  return `${base}${url}?api_key=${KEY}${options}`
-}
-
-function championById (summoner, id) {
-  const url = prepUrl(`/v1.2/champion/${id || summoner.match.championId}`, null, GLOBAL_BASE, '&champData=blurb,image')
-
-  return fetch(url)
-    .then(data => Object.assign(summoner, { champion: data }))
-}
-
-function gameBySummoner (summoner, region) {
-  const url = prepUrl(`/v1.3/game/by-summoner/${summoner.id}/recent`, region)
-
-  return fetch(url)
-    .then(data => Object.assign(summoner, { match: data.games[0] }))
-}
-
-function summonerByIds (team, summoners, region) {
-  const url = prepUrl(`/v1.4/summoner/${summoners}`, region)
-
-  if (_.isEmpty(team)) {
-    return new Promise(resolve => resolve([{}]))
-  }
-
-  return fetch(url)
-    .then(data => Object.assign(team, { roster: data }))
-}
-
-function summonerByName (summonerName, region) {
-  const url = prepUrl(`/v1.4/summoner/by-name/${summonerName}`, region)
-
-  return fetch(url)
-}
-
-function summonerStatsSummaryById (summoner, region) {
-  const url = prepUrl(`/v1.3/stats/by-summoner/${summoner.id}/summary`, region)
-
-  return fetch(url)
-    .then(data => Object.assign(summoner, { stats: data.playerStatSummaries }))
-}
-
-function topThreeChampions (summoner, region, platform) {
-  const url = prepUrl(`/championmastery/location/${platform}/player/${summoner.id}/topchampions`, null, 'https://eune.api.pvp.net')
-
-  return fetch(url)
-}
-
-function championMasteryAll (summoner, region, platform) {
-  const url = prepUrl(`/championmastery/location/${platform}/player/${summoner.id}/champions`, null, 'https://eune.api.pvp.net')
-
-  return fetch(url)
-}
-
-function masteryScore (summoner, region, platform) {
-  const url = prepUrl(`/championmastery/location/${platform}/player/${summoner.id}/score`, null, 'https://eune.api.pvp.net')
-
-  return fetch(url)
-}
-
+/**
+ * Get all champions
+ * @param  {bool} freeToPlay
+ * @return {Promise}
+ */
 function champions (freeToPlay) {
   const options = freeToPlay ? '&freeToPlay=true' : null
   const url = prepUrl('/v1.2/champion', null, BASE, options)
@@ -77,13 +18,77 @@ function champions (freeToPlay) {
   return fetch(url)
 }
 
-function teamBySummonerId (summoner, region) {
-  const url = prepUrl(`/v2.4/team/by-summoner/${summoner.id}`, region)
+/**
+ * Get a champion by id and append it to the provided object
+ * @param  {object} summoner
+ * @param  {string} id
+ * @return {Promise}
+ */
+function championById (summoner, id) {
+  const url = prepUrl(`/v1.2/champion/${id || summoner.match.championId}`, null, GLOBAL_BASE, '&champData=blurb,image')
 
   return fetch(url)
-    .then(data => Object.assign(summoner, { teams: data[summoner.id] }))
+    .then(data => Object.assign(summoner, { champion: data }))
 }
 
+/**
+ * Get all champoion masteries for a summoner
+ * @param  {object} summoner
+ * @param  {string} region
+ * @param  {string} platform
+ * @return {Promise}
+ */
+function championMasteryAll (summoner, region, platform) {
+  const url = prepUrl(`/championmastery/location/${platform}/player/${summoner.id}/champions`, null, 'https://eune.api.pvp.net')
+
+  return fetch(url)
+}
+
+/**
+ * Get a game by summoner ID and append it to the provided object
+ * @param  {object} summoner
+ * @param  {string} region
+ * @return {Promise}
+ */
+function gameBySummoner (summoner, region) {
+  const url = prepUrl(`/v1.3/game/by-summoner/${summoner.id}/recent`, region)
+
+  return fetch(url)
+    .then(data => Object.assign(summoner, { match: data.games[0] }))
+}
+
+/**
+ * Get league and division statistics
+ * @param  {object} summoner
+ * @param  {string} region
+ * @return {Promise}
+ */
+function league (summoner, region) {
+  const url = prepUrl(`/v2.5/league/by-summoner/${summoner.id}/entry`, region)
+
+  return fetch(url)
+    .then(league => league[summoner.id][0])
+}
+
+/**
+ * Get total mastery score
+ * @param  {object} summoner
+ * @param  {string} region
+ * @param  {string} platform
+ * @return {Promise}
+ */
+function masteryScore (summoner, region, platform) {
+  const url = prepUrl(`/championmastery/location/${platform}/player/${summoner.id}/score`, null, 'https://eune.api.pvp.net')
+
+  return fetch(url)
+}
+
+/**
+ * Get match statistics and append it to the provided match
+ * @param  {object} match
+ * @param  {string} region
+ * @return {Promise}
+ */
 function match (match, region) {
   const url = prepUrl(`/v2.2/match/${match.matchId}`, region)
 
@@ -91,6 +96,12 @@ function match (match, region) {
     .then(data => Object.assign(match, { match: data }))
 }
 
+/**
+ * Get ranked matches from the last 24 hours
+ * @param  {object} summoner
+ * @param  {string} region
+ * @return {Promise}
+ */
 function rankedMatches (summoner, region) {
   const time = moment()
     .subtract(1, 'day')
@@ -104,19 +115,85 @@ function rankedMatches (summoner, region) {
   return fetch(url)
 }
 
+/**
+ * Get server statuses for a region
+ * @param  {string} region
+ * @return {Promise}
+ */
 function status (region) {
   const url = prepUrl(`/shards/${region.toLowerCase()}`, null, 'http://status.leagueoflegends.com')
 
   return fetch(url)
 }
 
-function league (summoner, region) {
-  const url = prepUrl(`/v2.5/league/by-summoner/${summoner.id}/entry`, region)
+/**
+ * Get summoners by IDs and append it to the provided object
+ * @param  {object} tea
+ * @param  {string} summoners
+ * @param  {string} region
+ * @return {Promise}
+ */
+function summonerByIds (team, summoners, region) {
+  const url = prepUrl(`/v1.4/summoner/${summoners}`, region)
+
+  if (_.isEmpty(team)) {
+    return new Promise(resolve => resolve([{}]))
+  }
 
   return fetch(url)
-    .then(league => league[summoner.id][0])
+    .then(data => Object.assign(team, { roster: data }))
 }
 
+/**
+ * Get summoner information by name
+ * @param  {string} summonerName
+ * @param  {string} region
+ * @return {Promise}
+ */
+function summonerByName (summonerName, region) {
+  const url = prepUrl(`/v1.4/summoner/by-name/${summonerName}`, region)
+
+  return fetch(url)
+}
+
+/**
+ * Get summoner stats and append it to the provided object
+ * @param  {object} summoner
+ * @param  {string} region
+ * @return {Promise}
+ */
+function summonerStatsSummaryById (summoner, region) {
+  const url = prepUrl(`/v1.3/stats/by-summoner/${summoner.id}/summary`, region)
+
+  return fetch(url)
+    .then(data => Object.assign(summoner, { stats: data.playerStatSummaries }))
+}
+
+/**
+ * Get teams via a summoner id and append it to the provided object
+ * @param  {object} summoner
+ * @param  {string} region
+ * @return {Promise}
+ */
+function teamBySummonerId (summoner, region) {
+  const url = prepUrl(`/v2.4/team/by-summoner/${summoner.id}`, region)
+
+  return fetch(url)
+    .then(data => Object.assign(summoner, { teams: data[summoner.id] }))
+}
+
+/**
+ * Get top three champions of a summoner
+ * @param  {object} summoner
+ * @param  {string} region
+ * @param  {string} platform
+ * @return {Promise}
+ */
+function topThreeChampions (summoner, region, platform) {
+  const url = prepUrl(`/championmastery/location/${platform}/player/${summoner.id}/topchampions`, null, 'https://eune.api.pvp.net')
+
+  return fetch(url)
+}
 
 module.exports = {
   championById: championById,
---
+++ utils/prepUrl.js
@@ -0,0 +1,14 @@
+const BASE = process.env.LOL_BASE
+const GLOBAL_BASE = process.env.LOL_GLOBAL
+const KEY = process.env.RIOT_API_KEY
+
+function prepUrl (url, region, base, options) {
+  region = region || 'eune'
+  base = base || BASE
+  options = options || ''
+
+  base = base.replace('{region}', region)
+  return `${base}${url}?api_key=${KEY}${options}`
+}
+
+module.exports = prepUrl
